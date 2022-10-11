3d31c615512a6a1453391f1afaa3bad329214dc1
thanx12345 <thanx.hello12345@gmail.com>
2022-10-11 13:27:58 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネント
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,5 +1,7 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー１</x-slot>
 コンポーネントテスト１
-    <x-tests.card title="タイトル" content="本文" :message="$message"></x-tests.card>
+    <x-tests.card title="タイトル1" content="本文" :message="$message"></x-tests.card>
+    <x-tests.card title="タイトル2" />
+    <x-tests.card title="CSSを変更したい" class="bg-red-300"/>
 </x-tests.app>
