319109dad8ac785bcc2dc3f6d387e8a622091ff4
thanx12345 <thanx.hello12345@gmail.com>
2022-10-13 11:06:04 +0900

    serviceProviderTest

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -6,6 +6,15 @@ use Illuminate\Http\Request;
 
 class LifeCycleTestController extends Controller
 {
+    public function showServiceProviderTest(){
+        $encrypt = app()->make('encrypter');
+        $password = $encrypt->encrypt('password');
+
+        $sample = app()->make('serviceProviderTest');
+
+        dd($sample, $password, $encrypt->decrypt($password));
+    }
+
     public function showServiceContainerTest(){
         
         app()->bind('lifeCycleTest', function(){
@@ -13,6 +22,33 @@ class LifeCycleTestController extends Controller
         });
 
         $test = app()->make('lifeCycleTest');
+
+        // サービスコンテナ無しの場合
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+
+        // サービスコンテナapp()を使う場合
+        app()->bind('sample', Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
+
         dd($test, app());
+    }
+}
+
+class Sample
+{
+ public $message;
+ public function __construct(Message $message){
+ $this->message = $message;
+ }
+ public function run(){ $this->message->send(); }
+} 
+
+class Message
+{
+    public function send(){
+         echo('メッセージ表示'); 
     }
 }
