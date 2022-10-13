047c76b15061774d20739ea55d9b0042ada39f4b
thanx12345 <thanx.hello12345@gmail.com>
2022-10-13 17:46:02 +0900

    multiLogin

--- routes/admin.php
+++ routes/admin.php
@@ -22,12 +22,12 @@ use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    return view('admin.dashboard');
+})->middleware(['auth:admin'])->name('dashboard');
 
 
 Route::middleware('guest')->group(function () {
@@ -54,7 +54,7 @@ Route::middleware('guest')->group(function () {
                 ->name('password.update');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth:admin')->group(function () {
     Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
                 ->name('verification.notice');
 
